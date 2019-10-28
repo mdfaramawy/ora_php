478956597b696c06f0dcd52478e1522f9855dad4
Md Faramawy <mdfaramawy@gmail.com>
2019-10-28 14:43:03 +0200

    fix  error in connection string

--- config/database.php
+++ config/database.php
@@ -2,18 +2,22 @@
 class Database{
 	// specify your own database credentials
 	private $host     = "localhost";
-    private $db_name  = "PROD";
-    private $username = "hr";
-    private $password = "hr";
-    public  $conn;
+	private $db_name  = "prod";
+	private $username = "hr";
+	private $password = "hr";
+	public  $conn;
 	
 	// get the database connection
-    public function getConnection(){
+        public function getConnection(){
 		$this->conn = null;
 		try{
 			$this->conn = oci_connect($this->username, 
 			                          $this->password, 
-									  $this->db_name, 'AL32UTF8');
+						  '(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)
+						                             (HOST = '.$this->host.')
+						  			     (PORT = 1521)) 
+									     (CONNECT_DATA = (SERVICE_NAME = '.$this->db_name.') 
+									     (SID = '.$this->db_name.')))');
 			}
 		catch(Exception $exception){
 			echo "Connection error: " . $exception->getMessage();
@@ -21,4 +25,3 @@ class Database{
 			return $this->conn;
 			}
 }
-?>
